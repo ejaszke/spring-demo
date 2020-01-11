770545ad54b06850bf18b90397ce9942b592a00e
Edward Jaszke <edward.jaszke@imagene.me>
2020-01-11 19:04:19 +0100

    Added scaffolded application with Spring Initializr start.spring.io

---
+++ src/test/java/pl/eddo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.eddo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
